c89f062e32f177dc7761b8f261f018a4f28e8c3d
Martin Castro <martin.castro@mews.com>
2025-09-26 09:07:03 +0200

    fix version

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 module "common" {
   source = "git@github.com:martincastrocm/tf-module-renovate-test.git?ref=v0.1.0"
 }
+
+terraform {
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.24.0"
+    }
+  }
+}
