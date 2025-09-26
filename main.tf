1f5683291e806e645bb230e926b89198cde7221b
Martin Castro <martin.castro@mews.com>
2025-09-26 11:07:43 +0200

    fix version

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ module "common" {
   source = "git@github.com:martincastrocm/tf-module-renovate-test.git?ref=v0.1.0"
 }
 
+module "avm_utl_regions" {
+  source  = "Azure/avm-utl-regions/azurerm"
+  version = "0.5.0"
+}
+
 terraform {
   required_version = "~> 1.12.0"
 
@@ -9,6 +14,10 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.24.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.5"
     }
   }
 }
