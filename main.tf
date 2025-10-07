5c6eddea1d53a59617fc63e19a03267b3e7e7f05
Martin Castro <martin.castro@mews.com>
2025-10-07 18:22:36 +0200

    fix renovate config

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.24.0"
+      version = "~> 3.117.0"
     }
     azapi = {
       source  = "azure/azapi"
