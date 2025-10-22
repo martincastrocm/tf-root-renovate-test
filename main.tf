b43fc005bb2824df7edec1c478e2786d4ec302be
Martin Castro <martin.castro@mews.com>
2025-10-22 07:52:08 +0200

    test new renovate test config

--- main.tf
+++ main.tf
@@ -2,26 +2,35 @@ module "common" {
   source = "git@github.com:martincastrocm/tf-module-renovate-test.git?ref=v0.1.0"
 }
 
+module "commo2" {
+  source = "git@github.com:martincastrocm/tf-module-renovate-test-2.git?ref=v0.1.0"
+}
+
+
 module "avm_utl_regions" {
   source  = "Azure/avm-utl-regions/azurerm"
   version = "0.5.0"
 }
 
 terraform {
-  required_version = "~> 1.11.0"
+  required_version = "~> 1.12"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.117.0"
+      version = "~> 4.26"
     }
     azapi = {
-      source  = "azure/azapi"
-      version = "~> 2.5.0"
+      source  = "Azure/azapi"
+      version = "~> 2.5"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.3"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.6.0"
+      version = "~> 3.6"
     }
 
   }
